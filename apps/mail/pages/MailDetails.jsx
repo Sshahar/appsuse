b5d11b4c140c16cc9b9cb8a08a9136f14aefc180
Shahar Margalit <shaharma@gmail.com>
2025-01-22 17:52:08 +0200

    Style mail-details component

--- apps/mail/pages/MailDetails.jsx
+++ apps/mail/pages/MailDetails.jsx
@@ -1,3 +1,4 @@
+import { utilService } from "../../../services/util.service.js"
 import { mailService } from "../services/mail.service.js"
 
 const { useParams, Link } = ReactRouterDOM
@@ -5,7 +6,7 @@ const { useState, useEffect, useRef } = React
 
 export function MailDetails() {
     const [mail, setMail] = useState(null)
-    const {mailId} = useParams()
+    const { mailId } = useParams()
 
     useEffect(() => {
         loadMail()
@@ -19,11 +20,49 @@ export function MailDetails() {
             })
     }
 
+    const fromToStyle = { "margin": "1em", "display": "grid", "gridTemplateColumns": "1fr 1fr" }
+
+    const bodyStyle = {
+        fontSize: '1.3em',
+    }
+
+    const toStyle = {
+        'gridColumn': '1'
+    }
+
+    const dateStyle = {
+        'gridRow': '1',
+        'gridColumn': '2',
+    }
+
     if (!mail) return <div>Loading...</div>
     return (
         <React.Fragment>
-            <div>Mail Details</div>
-            {JSON.stringify(mail)}
+            {/* Subject*/}
+            {/* (TODO: add labels such as 'inbox') */}
+            <h2>{mail.subject}</h2>
+            {/* from, to, */}
+            <section style={fromToStyle}>
+                {/* From */}
+                <div>
+                    <span>From: {mail.from}</span>
+                </div>
+                {/* To */}
+                <div style={toStyle}>
+                    {/* {mail.to} */}
+                    <span>to me</span>
+                </div>
+                {/* Date */}
+                <span style={dateStyle}>{utilService.getLocaleDate(mail.sentAt)}</span>
+                {/* Star toggle button */}
+                {/* Reply */}
+
+            </section>
+            {/* body */}
+            <section style={bodyStyle}>
+                {mail.body}
+            </section>
+            {/* {JSON.stringify(mail)} */}
         </React.Fragment>
     )
 }
