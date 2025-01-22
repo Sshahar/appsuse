a92a40375a85a03e477f91ecc98f372d0ae9f847
Shahar Margalit <shaharma@gmail.com>
2025-01-22 14:19:06 +0200

    Add grid to mail preview

--- apps/mail/cmps/MailPreview.jsx
+++ apps/mail/cmps/MailPreview.jsx
@@ -1,19 +1,21 @@
 import { utilService } from '../../../services/util.service.js'
 
 export function MailPreview({ mail }) {
-    const selectStyle = {
+    const iconStyle = {
         "height": "20px",
         "width": "20px",
-        "border": "1px solid black",
     }
 
-    const clearStyle = {
-        "all": "unset",
-    }
+    const selectStyle = { "order": "0", "padding": "0 10px 0 13px", "position": "relative", "width": "20px" }
+
+    const starStyle = {"padding":"0 10px 0 0","WebkitBoxOrdinalGroup":"1","WebkitOrder":"0","order":"0","width":"20px"}
 
     const previewStyle = {
-        "display": "flex",
+        "display": "grid",
+        "gridAutoFlow": "column",
+        "gridTemplateColumns": "20px 20px 1fr 1fr 1fr",
         "gap": "10px",
+        "backgroundColor": "#F2F6FC",
     }
 
     return (
@@ -22,9 +24,9 @@ export function MailPreview({ mail }) {
                 {/* Select */}
                 <input type="checkbox" name="mail.id" style={selectStyle} />
                 {/* Star */}
-                <button style={clearStyle}>
-                    <img src="apps/mail/assets/star-not.svg" />
-                </button>
+                <span style={starStyle}>
+                    <img style={iconStyle} src="apps/mail/assets/img/asset 19.png" />
+                </span>
                 {/* From */}
                 <span>{mail.from}</span>
                 {/* Subject */}
