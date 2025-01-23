85e946895e5169f8e9c31d5212dc6beefaec68e9
Shahar Margalit <shaharma@gmail.com>
2025-01-23 11:56:22 +0200

    Fix inbox button toggles mail list view

--- apps/mail/cmps/MailFolderList.jsx
+++ apps/mail/cmps/MailFolderList.jsx
@@ -2,16 +2,16 @@ const { useLocation, useNavigate } = ReactRouter
 
 export function MailFolderList({setCmpType}) {
 
-    function onNavCompose() {
-        setCmpType('compose')
+    function onNav(to) {
+        setCmpType(to)
     }
     // Allow filtering by different folders: inbox / sent / trash/ draft
 
     return (<div className="main-folder-list">
-        <div onClick={onNavCompose} to='compose'>
+        <div onClick={() => onNav('compose')}>
             <img className="icon" src="assets/img/mail/compose.png" />Compose
         </div>
-        <div className="folder"><img className="icon" src='assets/img/mail/asset 35.png' /> inbox</div>
+        <div onClick={() => onNav('list')} className="folder"><img className="icon" src='assets/img/mail/asset 35.png' /> inbox</div>
         <div className="folder">sent</div>
         <div className="folder">trash</div>
         <div className="folder">draft</div>
