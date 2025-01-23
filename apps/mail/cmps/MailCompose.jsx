e2b2487166da1c77ae32c4f9b1b2d7e5361b53e0
Shahar Margalit <shaharma@gmail.com>
2025-01-23 09:52:00 +0200

    Add mail compose layout

--- apps/mail/cmps/MailCompose.jsx
+++ apps/mail/cmps/MailCompose.jsx
@@ -1,4 +1,25 @@
-export function MailCompose({mails, setCmpType}) {
-    
-    return <div>Mail Compose</div>
+export function MailCompose({ mails, setCmpType }) {
+
+    return (
+        <div className="mail-compose">
+            <form className="main-compose-form">
+                {/* To */}
+                <div>
+                    <label htmlFor="">To: </label>
+                    <input type="text" />
+                </div>
+                {/* Subject */}
+                <div>
+                    <label htmlFor="">Subject: </label>
+                    <input type="text" />
+                </div>
+                {/* Body */}
+
+                <textarea></textarea><br />
+                {/* Send */}
+                <button>Send</button>
+            </form>
+
+        </div>
+    )
 }
