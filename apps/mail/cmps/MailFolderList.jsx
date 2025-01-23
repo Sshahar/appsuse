aea6af00903d8caa0b7d7f8da80d67e272bdc0c5
Shahar Margalit <shaharma@gmail.com>
2025-01-23 13:01:13 +0200

    Add mail sent icon

--- apps/mail/cmps/MailFolderList.jsx
+++ apps/mail/cmps/MailFolderList.jsx
@@ -1,6 +1,6 @@
 const { useLocation, useNavigate } = ReactRouter
 
-export function MailFolderList({setCmpType}) {
+export function MailFolderList({ setCmpType }) {
 
     function onNav(to) {
         setCmpType(to)
@@ -11,8 +11,8 @@ export function MailFolderList({setCmpType}) {
         <div onClick={() => onNav('compose')}>
             <img className="icon" src="assets/img/mail/compose.png" />Compose
         </div>
-        <div onClick={() => onNav('list')} className="folder"><img className="icon" src='assets/img/mail/asset 35.png' /> inbox</div>
-        <div className="folder">sent</div>
+        <div onClick={() => onNav('list')} className="folder"><img className="icon" src='assets/img/mail/asset 35.png' />inbox</div>
+        <div onClick={() => onNav('list')} className="folder"><img className="icon" src='assets/img/mail/sent.svg' />sent</div>
         <div className="folder">trash</div>
         <div className="folder">draft</div>
     </div>)
