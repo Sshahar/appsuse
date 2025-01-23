89bbdb1628b6ae1f48bfc6a4b905a7e43b3d88a3
Shahar Margalit <shaharma@gmail.com>
2025-01-23 11:41:41 +0200

    Style convensions

--- apps/mail/cmps/MailList.jsx
+++ apps/mail/cmps/MailList.jsx
@@ -1,12 +1,11 @@
 import { MailPreview } from "./MailPreview.jsx"
 
 // export function MailList({mails, setCmpType}) {
-    export function MailList(props) {
-    console.log('props:', props)
+export function MailList(props) {
     return (
         <React.Fragment>
             {props.mails.map(mail => (
-                <MailPreview key={mail.id} mail={mail} setCmpType={props.setCmpType}/>
+                <MailPreview key={mail.id} mail={mail} setCmpType={props.setCmpType} />
             ))
             }
         </React.Fragment>
