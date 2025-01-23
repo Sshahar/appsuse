de57113e0bd4ad3e0465ed90c69713eaba0154a8
Shahar Margalit <shaharma@gmail.com>
2025-01-23 12:53:22 +0200

    Feat dynamic logo

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -1,16 +1,40 @@
+import { CustomNavLink } from "./CustomNavLink.jsx"
+
 const { Link, NavLink } = ReactRouterDOM
+const { useState, useEffect, useRef } = React
 
 export function AppHeader() {
+    const [logoType, setLogoType] = useState('mail')
 
-    return <header className="app-header">
+    return (<header className="app-header">
         <Link to="/">
-            <h3>LOGO!</h3>
+            <DynamicLogo cmpType={logoType} />
         </Link>
         <nav>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="/mail">Mail</NavLink>
-            <NavLink to="/note">Note</NavLink>
+            <NavLink to="/" onClick={() => setLogoType('default')}>Home</NavLink>
+            <NavLink to="/about" onClick={() => setLogoType('default')}>About</NavLink>
+            <NavLink onClick={() => setLogoType('mail')} to="/mail">Mail</NavLink>
+            <NavLink onClick={() => setLogoType('note')} to="/note">Note</NavLink>
         </nav>
-    </header>
+    </header>)
+}
+
+
+function DynamicLogo(props) {
+    // console.log('props:', props)
+    switch (props.cmpType) {
+        case 'default':
+        case 'note':
+            return <DefaultLogo {...props} />
+        case 'mail':
+            return <MailLogo {...props} />
+    }
+}
+
+function DefaultLogo(props) {
+    return (<h3>LOGO!</h3>)
+}
+
+function MailLogo(props) {
+    return (<h3><img src="assets/img/mail/logo.png" /></h3>)
 }
