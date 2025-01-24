cd25d2639074ad443429305a11e94abfb9aeb1d3
Shahar Margalit <shaharma@gmail.com>
2025-01-25 01:30:03 +0200

    Add bootstrap, style home page

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,6 +1,38 @@
+const { Container, Row, Col, Button } = ReactBootstrap
+const { Link } = ReactRouterDOM
+
+// export function Home() {
+//     return <section className="home">
+//         <h1>Welcome to the home page!</h1>
+//         <Button variant="info">Click Me</Button>
+
+//     </section>
+// }
 
 export function Home() {
-    return <section className="home">
-        <h1>Welcome to the home page!</h1>
+    return (<section className="home">
+        <div className="bg-light py-5">
+            <Container>
+                <Row className="align-items-center">
+                    <Col md={6}>
+                        <h1 className="display-4">Manage Everything in One Place</h1>
+                        <p className="lead">
+                            Simplify your life with our integrated platform. Stay on top of your emails, reading list, and notes—all from one app.
+                        </p>
+                        <Link to='/about'>
+                            <Button variant="primary" size="lg">Get Started</Button>
+                        </Link>
+                    </Col>
+                    <Col md={6}>
+                        <img
+                            src="assets/img/big-logo.png"
+                            alt="Appsuse Illustration"
+                            className="img-fluid"
+                        />
+                    </Col>
+                </Row>
+            </Container>
+        </div>
     </section>
-}
+    );
+};
