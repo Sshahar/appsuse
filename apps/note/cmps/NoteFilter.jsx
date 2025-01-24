ce2ee96167e4581c507e50fae36379fd6b3fede4
lironhefcode <lironheftman@gmail.com>
2025-01-24 12:49:58 +0200

    added duplication fillter by type and logo

---
+++ apps/note/cmps/NoteFilter.jsx
@@ -0,0 +1,27 @@
+
+
+export function NoteFilter({onSetFilter,filter}) {
+
+
+
+    return (
+        <div className="note-filter-contanier">
+            <button className={(filter==='NoteTxt')? 'active':undefined} onClick={(() => onSetFilter('NoteTxt'))}>
+            <svg  className='txt-filter' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                <path d="M32 32C14.3 32 0 46.3 0 64S14.3 96 32 96l128 0 0 352c0 17.7 14.3 32 32 32s32-14.3 32-32l0-352 128 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L192 32 32 32z" />
+            </svg>
+            </button>
+            <button  className={(filter==='NoteTodos')? 'active':undefined} onClick={(() => onSetFilter('NoteTodos'))}>
+            <svg className='todos-filter'  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                <path d="M40 48C26.7 48 16 58.7 16 72l0 48c0 13.3 10.7 24 24 24l48 0c13.3 0 24-10.7 24-24l0-48c0-13.3-10.7-24-24-24L40 48zM192 64c-17.7 0-32 14.3-32 32s14.3 32 32 32l288 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L192 64zm0 160c-17.7 0-32 14.3-32 32s14.3 32 32 32l288 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-288 0zm0 160c-17.7 0-32 14.3-32 32s14.3 32 32 32l288 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-288 0zM16 232l0 48c0 13.3 10.7 24 24 24l48 0c13.3 0 24-10.7 24-24l0-48c0-13.3-10.7-24-24-24l-48 0c-13.3 0-24 10.7-24 24zM40 368c-13.3 0-24 10.7-24 24l0 48c0 13.3 10.7 24 24 24l48 0c13.3 0 24-10.7 24-24l0-48c0-13.3-10.7-24-24-24l-48 0z" />
+            </svg>
+            </button>
+            <button className={(filter==='NoteImg')? 'active':undefined} onClick={(() => onSetFilter('NoteImg'))}>
+            <svg className='img-filter' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                <path d="M0 96C0 60.7 28.7 32 64 32l384 0c35.3 0 64 28.7 64 64l0 320c0 35.3-28.7 64-64 64L64 480c-35.3 0-64-28.7-64-64L0 96zM323.8 202.5c-4.5-6.6-11.9-10.5-19.8-10.5s-15.4 3.9-19.8 10.5l-87 127.6L170.7 297c-4.6-5.7-11.5-9-18.7-9s-14.2 3.3-18.7 9l-64 80c-5.8 7.2-6.9 17.1-2.9 25.4s12.4 13.6 21.6 13.6l96 0 32 0 208 0c8.9 0 17.1-4.9 21.2-12.8s3.6-17.4-1.4-24.7l-120-176zM112 192a48 48 0 1 0 0-96 48 48 0 1 0 0 96z" />
+            </svg>
+            </button>
+        </div>
+    )
+
+}
--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -23,11 +23,13 @@ export function AppHeader() {
 function DynamicLogo(props) {
     // console.log('props:', props)
     switch (props.cmpType) {
-        case 'default':
+        
         case 'note':
-            return <DefaultLogo {...props} />
+            return <NoteLogo {...props} />
         case 'mail':
             return <MailLogo {...props} />
+
+        default: return <DefaultLogo {...props}/>
     }
 }
 
@@ -37,4 +39,7 @@ function DefaultLogo(props) {
 
 function MailLogo(props) {
     return (<h3><img src="assets/img/mail/logo.png" /></h3>)
+}
+function NoteLogo(props){
+    return (<h3 className="keep-logo"><img src="apps\note\assets\keep.png" /></h3>)
 }
