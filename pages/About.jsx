3d9ed4548fb24b1cd9b48b69e945b9070a91a85f
Shahar Margalit <shaharma@gmail.com>
2025-01-25 01:09:05 +0200

    Add about page

--- pages/About.jsx
+++ pages/About.jsx
@@ -1,6 +1,14 @@
 
 export function About() {
     return <section className="about">
-        <h1>About Page</h1>
+        <h1>About Appsuse</h1>
+        <div className="centered-text">
+            <span>
+                Appsuse is a versatile single-page application designed to streamline your everyday tasks by integrating three core services into one easy-to-use platform. Whether you're managing emails, keeping notes, or organizing your reading list, Appsuse brings it all together seamlessly.
+            </span>
+        </div>
+        <div className="about-hero">
+            <img src="assets/img/big-logo.png" />
+        </div>
     </section>
 }
