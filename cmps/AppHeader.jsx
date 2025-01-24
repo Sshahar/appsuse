74caee5bfa0df15f4d79d2d7cc7abc311966e7ca
Shahar Margalit <shaharma@gmail.com>
2025-01-24 12:09:35 +0200

    Minor header styling

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -7,9 +7,12 @@ export function AppHeader() {
     const [logoType, setLogoType] = useState('default')
 
     return (<header className="app-header">
+        <div className="logo-items">
+        <svg className="icon" focusable="false" viewBox="0 0 24 24"><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path></svg>
         <Link to="/">
             <DynamicLogo cmpType={logoType} />
         </Link>
+        </div>
         <nav>
             <NavLink to="/" onClick={() => setLogoType('default')}>Home</NavLink>
             <NavLink to="/about" onClick={() => setLogoType('default')}>About</NavLink>
