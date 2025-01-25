6cfbcac88b2e6f845692b9178bdea4f70357a575
Shahar Margalit <shaharma@gmail.com>
2025-01-25 10:34:29 +0200

    Remove unusable hamburger icon

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -15,7 +15,6 @@ export function AppHeader() {
     return (<header className="app-header">
         <Link to="/">
             <div className="logo-items">
-                <svg className="icon" focusable="false" viewBox="0 0 24 24"><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path></svg>
                 <DynamicLogo cmpType={logoType} />
             </div>
         </Link>
