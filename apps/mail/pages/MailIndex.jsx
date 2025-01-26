4dcb6015f2bc23b72355972aa64238ee409a02a0
Shahar Margalit <shaharma@gmail.com>
2025-01-26 19:28:03 +0200

    Fix mail preview's text overflow

--- apps/mail/pages/MailIndex.jsx
+++ apps/mail/pages/MailIndex.jsx
@@ -84,19 +84,24 @@ export function MailIndex() {
                     <ul className="clean-list">
                         <li className="mail-preview">
                             {/* TODO: add drag and drop icon on hover */}
-                            {/* Select */}
-                            <input type="checkbox" />
-                            {/* Star */}
-                            <img className="icon" src={`${IMG_PATH}/starred.png`} />
-                            {/* Important */}
-                            <img className="icon" src={`${IMG_PATH}/important.png`} />
+                            {/* Left button group */}
+                            <div className="left-btns">
+                                {/* Select */}
+                                <input type="checkbox" />
+                                {/* Star */}
+                                <img className="icon" src={`${IMG_PATH}/starred.png`} />
+                                {/* Important */}
+                                <img className="icon" src={`${IMG_PATH}/important.png`} />
+                            </div>
                             {/* Name */}
                             <span className="name">h-ridee</span>
-                            {/* Subject */}
-                            <span className="subject">Nvidia Accounts -</span>
-                            {/* Body */}
-                            <span className="body">Hello,
-                                Your account just logged in using a(n) Windows device we don't</span>
+                            <div className="main-text">
+                                {/* Subject */}
+                                <span className="subject">Nvidia Accounts -</span>
+                                {/* Body */}
+                                <span className="body">Hello,
+                                    Your account just logged account just logged account just logged account just logged in using a(n) Windows device we don't</span>
+                            </div>
                             {/* Date  */}
                             <span className="date">29/12/2024</span>
                         </li>
