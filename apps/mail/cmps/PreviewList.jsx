91b53d982995678a4d7be231bde57fd7e03cfb2c
Shahar Margalit <shaharma@gmail.com>
2025-01-27 01:47:17 +0200

    Add PreviewList component

---
+++ apps/mail/cmps/PreviewList.jsx
@@ -0,0 +1,38 @@
+import { globalState } from "../services/globalState"
+
+const IMG_PATH = globalState.getImgPath()
+
+export function PreviewList() {
+    return (
+        <section>
+            <ul className="clean-list">
+                <li className="mail-preview">
+                    {/* TODO: add drag and drop icon on hover */}
+                    {/* Left button group */}
+                    <div className="left-btns">
+                        {/* Select */}
+                        <input type="checkbox" />
+                        {/* Star */}
+                        <img className="icon" src={`${IMG_PATH}/starred.png`} />
+                        {/* Important */}
+                        <img className="icon" src={`${IMG_PATH}/important.png`} />
+                    </div>
+                    {/* Name */}
+                    <span className="name">h-ridee</span>
+                    <div className="main-text">
+                        {/* Subject */}
+                        <span className="subject">Nvidia Accounts -
+                            {/* Body */}
+                            <span className="body">Hello,
+                                Your account just logged account just logged account just logged account just logged in using a(n) Windows device we don't
+                            </span>
+                        </span>
+
+                    </div>
+                    {/* Date  */}
+                    <span className="date">29/12/2024</span>
+                </li>
+            </ul>
+        </section>
+    )
+}
--- apps/mail/pages/MailIndex.jsx
+++ apps/mail/pages/MailIndex.jsx
@@ -1,11 +1,12 @@
 import { FolderList } from "../cmps/FolderList.jsx"
+import { PreviewList } from "../cmps/PreviewList.jsx"
 import { globalState } from "../services/globalState.js"
 
 
+const IMG_PATH = globalState.getImgPath()
 
 
 export function MailIndex() {
-    const IMG_PATH = globalState.getImgPath()
     return (
         <section className="mail-index">
             {/* Aside */}
@@ -58,35 +59,7 @@ export function MailIndex() {
                 </section>
 
                 {/* Preview list */}
-                <section>
-                    <ul className="clean-list">
-                        <li className="mail-preview">
-                            {/* TODO: add drag and drop icon on hover */}
-                            {/* Left button group */}
-                            <div className="left-btns">
-                                {/* Select */}
-                                <input type="checkbox" />
-                                {/* Star */}
-                                <img className="icon" src={`${IMG_PATH}/starred.png`} />
-                                {/* Important */}
-                                <img className="icon" src={`${IMG_PATH}/important.png`} />
-                            </div>
-                            {/* Name */}
-                            <span className="name">h-ridee</span>
-                            <div className="main-text">
-                                {/* Subject */}
-                                <span className="subject">Nvidia Accounts -
-                                    {/* Body */}
-                                    <span className="body">Hello,
-                                        Your account just logged account just logged account just logged account just logged in using a(n) Windows device we don't</span>
-                                </span>
-
-                            </div>
-                            {/* Date  */}
-                            <span className="date">29/12/2024</span>
-                        </li>
-                    </ul>
-                </section>
+                <PreviewList />
             </main>
         </section>
     )
