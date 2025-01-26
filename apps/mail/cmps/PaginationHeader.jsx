46a040f7e74e40787fa8341e4f23c07689706dc9
Shahar Margalit <shaharma@gmail.com>
2025-01-27 01:50:25 +0200

    Add PaginationHeader component

---
+++ apps/mail/cmps/PaginationHeader.jsx
@@ -0,0 +1,24 @@
+import { globalState } from "../services/globalState"
+
+const IMG_PATH = globalState.getImgPath()
+
+export function PaginationHeader() {
+    return (<form className="pagination-header">
+        {/* Select buttons */}
+        <section className="left-btns">
+            <div>
+                <input type="checkbox" />
+                <img className="icon" src={`${IMG_PATH}/checkbox-arrow.png`} />
+            </div>
+            <img className="icon" src={`${IMG_PATH}/refresh.png`} />
+            <img className="icon" src={`${IMG_PATH}/more.png`} />
+        </section>
+        {/* Pagination */}
+        <section className="right-btns">
+            <span>1–50 of 1,231</span>
+            <img className="icon" src={`${IMG_PATH}/newer.png`} />
+            <img className="icon" src={`${IMG_PATH}/older.png`} />
+        </section>
+    </form>
+    )
+}
--- apps/mail/pages/MailIndex.jsx
+++ apps/mail/pages/MailIndex.jsx
@@ -1,11 +1,7 @@
 import { FolderList } from "../cmps/FolderList.jsx"
 import { LabelsHeader } from "../cmps/LabelsHeader.jsx"
+import { PaginationHeader } from "../cmps/PaginationHeader.jsx"
 import { PreviewList } from "../cmps/PreviewList.jsx"
-import { globalState } from "../services/globalState.js"
-
-
-const IMG_PATH = globalState.getImgPath()
-
 
 export function MailIndex() {
     return (
@@ -16,23 +12,7 @@ export function MailIndex() {
             {/* Main */}
             <main>
                 {/* Pagination header */}
-                <form className="pagination-header">
-                    {/* Select buttons */}
-                    <section className="left-btns">
-                        <div>
-                            <input type="checkbox" />
-                            <img className="icon" src={`${IMG_PATH}/checkbox-arrow.png`} />
-                        </div>
-                        <img className="icon" src={`${IMG_PATH}/refresh.png`} />
-                        <img className="icon" src={`${IMG_PATH}/more.png`} />
-                    </section>
-                    {/* Pagination */}
-                    <section className="right-btns">
-                        <span>1–50 of 1,231</span>
-                        <img className="icon" src={`${IMG_PATH}/newer.png`} />
-                        <img className="icon" src={`${IMG_PATH}/older.png`} />
-                    </section>
-                </form>
+                <PaginationHeader />
                 {/* Labels header */}
                 <LabelsHeader />
 
