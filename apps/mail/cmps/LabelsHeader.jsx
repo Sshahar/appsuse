53f65e41a83cacdac124c6324a357f0e43b5e12a
Shahar Margalit <shaharma@gmail.com>
2025-01-27 17:09:18 +0200

    Dynamic render labels

--- apps/mail/cmps/LabelsHeader.jsx
+++ apps/mail/cmps/LabelsHeader.jsx
@@ -3,28 +3,22 @@ import { globalState } from "../services/globalState"
 const IMG_PATH = globalState.getImgPath()
 
 export function LabelsHeader() {
+
+    const labels = ['primary', 'promotions', 'social', 'updates', 'forums',]
+
+    function _getLabelImgSrc(label) {
+        if (label === 'primary') return 'inbox'
+        return label
+    }
+
     return (<section className="labels-header">
         <ul className="clean-list">
-            <li>
-                <img className="icon" src={`${IMG_PATH}/inbox.png`} />
-                <span className="capitalize">primary</span>
-            </li>
-            <li>
-                <img className="icon" src={`${IMG_PATH}/promotions.png`} />
-                <span className="capitalize">promotions</span>
-            </li>
-            <li>
-                <img className="icon" src={`${IMG_PATH}/social.png`} />
-                <span className="capitalize">social</span>
-            </li>
-            <li>
-                <img className="icon" src={`${IMG_PATH}/updates.png`} />
-                <span className="capitalize">updates</span>
-            </li>
-            <li>
-                <img className="icon" src={`${IMG_PATH}/forums.png`} />
-                <span className="capitalize">forums</span>
-            </li>
+            {labels.map(label => (
+                <li>
+                    <img className="icon" src={`${IMG_PATH}/${_getLabelImgSrc(label)}.png`} />
+                    <span className="capitalize">{label}</span>
+                </li>
+            ))}
         </ul>
     </section>
     )
