63aeca83c29918d57fd3b4a94fa7ba63fa1d33ce
Shahar Margalit <shaharma@gmail.com>
2025-01-27 22:33:38 +0200

    Add mail compose UI

--- apps/mail/cmps/MailCompose.jsx
+++ apps/mail/cmps/MailCompose.jsx
@@ -9,10 +9,32 @@ export function MailCompose({ saveAndClose }) {
     }
 
     return (
-        <div className="mail-compose">Mail compose
-            <button className="clean-btn" onClick={onSaveAndClose}>
-                <img src={`${IMG_PATH}/save-and-close.png`} alt="" />
-            </button>
+        <div className="mail-compose">
+            {/* Header */}
+            <section className="compose-header">
+                <span>New Message</span>
+                <button className="clean-btn" onClick={onSaveAndClose}>
+                    <img src={`${IMG_PATH}/save-and-close.png`} alt="" />
+                </button>
+            </section>
+
+            {/* Form */}
+            <form>
+                <div>
+                    <input autoFocus type="text" placeholder="Recipients"/>
+                </div>
+                <div>
+                    <input type="text" placeholder="Subject"/>
+                </div>
+                <div className="body">
+                    <textarea name="" id=""></textarea>
+                </div>
+
+                {/* Buttons */}
+                <section className="form-footer">
+                    <button className="clean-btn">Send</button>
+                </section>
+            </form>
         </div>
     )
 }
