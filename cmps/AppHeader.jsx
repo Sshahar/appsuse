4df0d42d533f82f823502976c1583c2c93ecf9b0
Shahar Margalit <shaharma@gmail.com>
2025-01-31 17:44:03 +0200

    Add ugly code for beautiful header navigation

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -1,7 +1,6 @@
-const { useEffect } = React
+const { useEffect, useState } = React
 
 const { Link, NavLink, useLocation } = ReactRouterDOM
-const { useState } = React
 
 // Dynamic app header
 export function AppHeader() {
@@ -12,90 +11,97 @@ export function AppHeader() {
         setHeaderState(() => location.pathname.split('/')[1])
     }, [location])
     switch (headerType) {
-        case 'mail':
-            return <MailHeader />
         default:
-            return <DefaultHeader />
+            return <MailHeader />
     }
 }
 
 function MailHeader() {
+    const [areAppsShown, setAreAppsShown] = useState(true) // TODO: change to false by default
+    const location = useLocation()
+    const [page, setPage] = useState(location.pathname.split('/')[1])
+
+    useEffect(() => {
+        setPage(location.pathname.split('/')[1])
+        console.log('page:', page)
+    }, [location])
+
+    function onToggleApps() {
+        setAreAppsShown(prev => !prev)
+    }
+
+    const apps = [
+        { name: 'book' },
+        { name: 'mail' },
+        { name: 'note' },
+    ]
+
+    const isDefaultPage = !['mail', 'note'].includes(page)
+
     return (
         <React.Fragment>
             <header className="mail-app-header">
-                <div className="group">
-                    {/* Hamburger */}
-                    <img className="hamburger" src="assets/img/hamburger.svg" />
-                    {/* Logo */}
-                    <img className="mail-logo" src="apps/mail/assets/img/logo.png" />
-                </div>
+                {page === 'mail' &&
+                    <React.Fragment>
+                        <div className="group">
+                            {/* Hamburger */}
+                            <img className="hamburger" src="assets/img/hamburger.svg" />
+                            {/* Logo */}
+                            <img className="mail-logo" src="apps/mail/assets/img/logo.png" />
+                        </div>
 
-                <div className="group search-group">
-                    {/* Search / filter */}
-                    <input className="mail-search clickable" type="text" placeholder="Search mail" />
-                    <button className="search-options"></button>
-                </div>
+                        <div className="group search-group">
+                            {/* Search / filter */}
+                            <input className="mail-search clickable" type="text" placeholder="Search mail" />
+                            <button className="search-options"></button>
+                        </div>
 
-                <div className="group">
-                    {/* Apps icons */}
-                    <img className="opacity-1 mail-icon2" src="assets/img/settings.svg" />
-                    <img className="opacity-1 mail-icon2" src="assets/img/apps.svg" />
-                    <img className="mail-icon3 circle" src="assets/img/profile.jpeg" />
-                </div>
+                        <div className="group">
+                            {/* Apps icons */}
+                            <img className="opacity-1 mail-icon2" src="assets/img/settings.svg" />
+                            <img onClick={onToggleApps} className="opacity-1 mail-icon2" src="assets/img/apps.svg" />
+                            <img className="mail-icon3 circle" src="assets/img/profile.jpeg" />
+                        </div>
+                    </React.Fragment>
+                }
+                {page === 'note' &&
+                    <React.Fragment>
+
+                        <NoteLogo />
+                        <img onClick={onToggleApps} className="right-group opacity-1 mail-icon2" src="assets/img/apps.svg" />
+                    </React.Fragment>
+                }
+
+                {isDefaultPage &&
+                    <React.Fragment>
+                        <DefaultLogo />
+                        <img onClick={onToggleApps} className="right-group opacity-1 mail-icon2" src="assets/img/apps.svg" />
+                    </React.Fragment>
+                }
+
+                {/* Apps display window */}
+                {areAppsShown &&
+                    <div className="apps-window">
+                        {apps.map(  a => (
+                            <NavLink key={a.name} className="app-link" to={`/${a.name}`}>
+                                <img className="icon-4" src={`assets/img/${a.name}.png`} />
+                                <span>{a.name}</span>
+                            </NavLink>
+                        ))}
+
+                    </div>
+                }
             </header>
+
 
         </React.Fragment>
     )
 }
 
-function DefaultHeader() {
-    const [logoType, setLogoType] = useState(getLogoType())
-
-    function getLogoType() {
-        const url = window.location.href
-        if (url.includes('mail')) return ('mail')
-        else if (url.includes('note')) return ('note')
-        else return ('default')
-    }
-    return (
-        <header className="app-header">
-            <Link to="/">
-                <div className="logo-items">
-                    <DynamicLogo cmpType={logoType} />
-                </div>
-            </Link>
-            <nav>
-                <NavLink to="/" onClick={() => setLogoType('default')}>Home</NavLink>
-                <NavLink to="/about" onClick={() => setLogoType('default')}>About</NavLink>
-                <NavLink onClick={() => setLogoType('mail')} to="/mail">Mail</NavLink>
-                <NavLink onClick={() => setLogoType('note')} to="/note">Note</NavLink>
-                <NavLink onClick={() => setLogoType('book')} to="/book">Book</NavLink>
-            </nav>
-        </header>
-    )
+function DefaultLogo() {
+    return (<h3 className="default-logo"><img src="assets/img/main-logo.png" alt="" /></h3>)
 }
 
-function DynamicLogo(props) {
-    // console.log('props:', props)
-    switch (props.cmpType) {
-
-        case 'note':
-            return <NoteLogo {...props} />
-        case 'mail':
-            return <MailLogo {...props} />
-
-        default: return <DefaultLogo {...props} />
-    }
-}
-
-function DefaultLogo(props) {
-    return (<h3><img src="assets/img/main-logo.png" alt="" /></h3>)
-}
-
-// TODO: remove when mail header is complete
-function MailLogo(props) {
-    return (<h3>Gmail!</h3>)
-}
 function NoteLogo(props) {
     return (<h3 className="keep-logo"><img src="apps\note\assets\keep.png" /></h3>)
 }
